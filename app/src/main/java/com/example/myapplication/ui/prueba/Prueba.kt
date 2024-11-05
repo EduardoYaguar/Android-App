4727ef9a8ab2ba5dc35cc85e910ffad15c4298e0
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2024-11-04 21:27:54 -0500

    App Structure

---
+++ app/src/main/java/com/example/myapplication/ui/prueba/Prueba.kt
@@ -0,0 +1,53 @@
+package com.example.myapplication.ui.prueba
+
+import android.content.ClipData.Item
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
+
+class Prueba : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_prueba)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val items = listOf("SEXO", "MACHO", "HEMBRA")
+
+        val adapter = ArrayAdapter(this , android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position != 0 ){
+                    val selectedItem = items[position]
+                    Toast.makeText(this@Prueba, "$selectedItem", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+    }
+}
